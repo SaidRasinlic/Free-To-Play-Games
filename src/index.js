3d9e178cba03e771737be3c705f9b4c773d9047b
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-05-19 14:37:18 +0200

    Update Meta data

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import './assets/images/image17.png';
 import './assets/images/batman7.png';
 import './assets/images/nfs.png';
 import './assets/images/bg999.png';
+import './assets/images/Netlify.png';
 
 const arrowRight = document.querySelector('.b-display-right');
 arrowRight.addEventListener('click', () => plusDivs(1));
